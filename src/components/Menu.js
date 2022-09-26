512a8c40085423ba9c7e25a7b46b376e6d7561a6
VitorMarceloSantos <vitor.marcelo2009@hotmail.com>
2022-09-26 17:10:37 -0300

    Requisito 11

---
+++ src/components/Menu.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import filtersSearch from '../functions/filtersSeach';
+
+function Menu() {
+  return (
+    <div>
+      {filtersSearch()}
+      Menu
+    </div>
+  );
+}
+
+export default Menu;
