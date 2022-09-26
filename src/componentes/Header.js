1683ecf07be1ddc0fca97fe8487c9299f908932c
FranciscoVieir <franciscovieira6397@gmail.com>
2022-09-26 16:46:33 -0300

    adicionando o requisito 9 e 10

---
+++ src/componentes/Header.js
@@ -0,0 +1,17 @@
+import React, { useContext } from 'react';
+import BarraDePesquisa from './BarraDePesquisa';
+import RadioInput from './RadioInput';
+import MyContext from '../context/Context';
+
+const Header = () => {
+  const { searchBar } = useContext(MyContext);
+
+  return (
+    <div>
+      {searchBar ? <BarraDePesquisa /> : ''}
+      <RadioInput />
+    </div>
+  )
+}
+
+export default Header;
