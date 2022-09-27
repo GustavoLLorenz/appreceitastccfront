ec1db035b4743b1b616685ce6f066de4f57e54f4
Alan-Lim4 <4lanc03lh0@gmail.com>
2022-09-27 16:56:20 -0300

    requisito 24 cria recipeDetails

---
+++ src/pages/RecipeDetails.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react';
+import { useLocation, useRouteMatch } from 'react-router-dom';
+import { getDrinkById, getMealById } from '../services/recipesDetails';
+
+function RecipeDetails() {
+  const [setRecipe] = useState(null);
+
+  const { pathname } = useLocation();
+  const { params } = useRouteMatch();
+  const { id } = params;
+
+  const detailsMeals = /^\/meals\/.*/i.test(pathname);
+  const detailsDrinks = /^\/drinks\/.*/i.test(pathname);
+
+  useEffect(() => {
+    if (detailsDrinks) getDrinkById(id).then(setRecipe);
+    else if (detailsMeals) getMealById(id).then(setRecipe);
+  }, [id, detailsDrinks, detailsMeals]);
+
+  return (
+    <header>
+      {detailsMeals && !detailsDrinks && <span>Meals</span>}
+      {detailsDrinks && !detailsMeals && <span>Drinks</span>}
+    </header>
+  );
+}
+
+export default RecipeDetails;
