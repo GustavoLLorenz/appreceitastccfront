be338ab6d5e6b6467b112dfb5e8d17ec64f74c5a
Alan-Lim4 <4lanc03lh0@gmail.com>
2022-09-27 17:43:10 -0300

    erro de lint, comentei setDatasearchDrink da barra de pesquisa

--- src/pages/RecipeDetails.jsx
+++ src/pages/RecipeDetails.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react';
 import { useLocation, useRouteMatch } from 'react-router-dom';
 import { getDrinkById, getMealById } from '../services/recipesDetails';
