5ba9d5e543a6141743b563fe0a85709b21e2e904
Lutti <lutti.c.amaral@gmail.com>
2022-09-27 14:03:27 -0300

    Parte do Requisito 14

---
+++ src/components/CardRecipe.js
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types';
+
+function CardRecipe({ imageSrc, index, name}) {
+  return (
+    <div data-testid={ `${index}-recipe-card` }>
+      <img
+        src={ imageSrc }
+        data-testid={ `${index}-card-img` }
+        alt={ `${index}-${name}-alt` }
+      />
+      <p data-testid={ `${index}-card-name` }>{ name }</p>
+    </div>
+  );
+}
+
+CardRecipe.propTypes = {
+  imageSrc: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  index: PropTypes.number.isRequired,
+};
+
+export default CardRecipe;
