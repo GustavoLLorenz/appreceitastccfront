525f3e564899739df51a3eeda492cbcf70eb8ba3
Alan-Lim4 <4lanc03lh0@gmail.com>
2022-09-27 21:57:08 -0300

    requisito 37

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import Profile from './pages/Profile';
 import DoneRecipes from './pages/DoneRecipes';
 import FavoriteRecipes from './pages/FavoriteRecipes';
 import RecipeDetails from './pages/RecipeDetails';
+import RecipeInProgress from './pages/RecipeInProgress';
 
 function App() {
   return (
@@ -22,8 +23,10 @@ function App() {
           <Route exact path="/" component={ TelaDeLogin } />
           <Route exact path="/meals" component={ Menu } />
           <Route exact path="/meals/:id" component={ RecipeDetails } />
+          <Route exact path="/meals/:id/in-progress" component={ RecipeInProgress } />
           <Route exact path="/drinks" component={ Drinks } />
           <Route exact path="/drinks/:id" component={ RecipeDetails } />
+          <Route exact path="/drinks/:id/in-progress" component={ RecipeInProgress } />
           <Route exact path="/profile" component={ Profile } />
           <Route exact path="/done-recipes" component={ DoneRecipes } />
           <Route exact path="/favorite-recipes" component={ FavoriteRecipes } />
