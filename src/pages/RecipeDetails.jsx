d6b70f9622c78ebfd4974ae601f8211185ac6de8
Gustavo Lopes Lorenz <gustavolorenz43@gmail.com>
2022-09-29 15:43:23 -0300

    exerc 23 erro de lint

--- src/pages/RecipeDetails.jsx
+++ src/pages/RecipeDetails.jsx
@@ -16,7 +16,7 @@ function RecipeDetails() {
   useEffect(() => {
     if (detailsDrinks) getDrinkById(id).then(setRecipe);
     else if (detailsMeals) getMealById(id).then(setRecipe);
-  }, [id, detailsDrinks, detailsMeals]);
+  }, [id, detailsDrinks, detailsMeals, setRecipe]);
 
   return (
     <header>
